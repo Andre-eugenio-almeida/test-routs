1e51b809daaf8e7999b6f93599318cb48357e603
André Almeida <almeida.ae@outlook.com>
2023-02-12 12:11:45 -0300

    Testando rota com o GET

--- src/index.js
+++ src/index.js
@@ -3,8 +3,23 @@ const app = express()
 const PORT = 3000
 
 app.get('/', (req, res) => {
-    console.log("Entrou Aqui!")
-    res.send("HELLO WORD!")
+    res.send("app GET, testando rota com  GET")
+})
+
+app.post('/', (req, res) => {
+    res.send("app POST, testando rota com POST")
+})
+
+app.put('/', (req, res) => {
+    res.send("app PUT, testando rota com PUT")
+})
+
+app.delete('/', (req, res) => {
+    res.send("app DELETE, testando rota com DELETE")
+})
+
+app.path('/', (req, res) => {
+    res,send("app PATH, testando rora com PATH")
 })
 
 app.listen(PORT, () => {
